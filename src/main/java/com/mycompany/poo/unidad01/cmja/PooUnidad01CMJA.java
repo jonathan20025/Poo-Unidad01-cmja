f44af214654c1993f68b6fcadb32144080faf482
PC-17 <PC-17@DESKTOP-QGSMH7J>
2022-03-30 13:35:52 +0200

    cambio el mensaje de hola mundo en español

---
+++ src/main/java/com/mycompany/poo/unidad01/cmja/PooUnidad01CMJA.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.poo.unidad01.cmja;
+
+/**
+ *
+ * @author PC-17
+ */
+public class PooUnidad01CMJA {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
